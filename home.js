14e7b5f7b363d58a09ea2a4681e928acd825fd20
pavel2644 <pavel2644@gmail.com>
2024-09-20 02:16:02 +0100

    update the main balance

--- home.js
+++ home.js
@@ -9,4 +9,28 @@ console.log(addAmount);
 // get the pin number provided
 const pinNnmber = document.getElementById('input-pin-number').value;
 console.log(pinNnmber); 
+// verify the pin number(wrong way)
+
+if(pinNnmber === '1234'){
+    console.log('Added money to your account') 
+
+// step 4: get the current balance
+const currentBalance = document.getElementById('balance').innerText;
+console.log(currentBalance)
+
+// add money input with current balance
+const addMoneyInput = parseFloat(addAmount);
+const balance = parseFloat(currentBalance);
+//console.log(addMoneyInput)
+//console.log(typeof addMoneyInput)
+const newBalance = addMoneyInput + balance;
+console.log(newBalance)
+
+// step 6: update the main balance
+document.getElementById('balance').innerText = newBalance;
+
+}
+else{
+    console.log("wrong pin number! Please try again")
+}
 })
